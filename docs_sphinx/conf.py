287d256858f2e601e0879d0f6921b6fca0d56efe
vrooomy <vj.bro.833@gmail.com>
2026-06-02 15:21:39 +0530

    closed irritating toggles

--- docs_sphinx/conf.py
+++ docs_sphinx/conf.py
@@ -160,7 +160,7 @@ html_theme_options = {
     "show_toc_level": 2,
     "navigation_with_keys": True,
     "show_prev_next": True,
-    "show_nav_level": 2,
+    "show_nav_level": 1,
     "navigation_depth": 4,
     "secondary_sidebar_items": {"**": ["page-toc"], "index": []},
     "back_to_top_button": True,
